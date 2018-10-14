b352901bbe96bc0d621945ec51fa899b2bec6ee7
453slavchic <43350703+453slavchic@users.noreply.github.com>
2018-10-14 14:07:57 +0300

    Create ex4.py

---
+++ Homework2/ex4.py
@@ -0,0 +1,15 @@
+# Упражнение 4
+# Программа сначала получает на вход строку S, а затем натуральное число N. На выходе должна быть такая строка R, что каждый символ из S повторяется N раз.
+
+def multiply(s, n):
+    r = ''
+    for char in s:
+        for i in range(n):
+            r = r + char
+    return r
+
+
+S = str(input())
+N = int(input())
+R = multiply(S, N)
+print(R)
