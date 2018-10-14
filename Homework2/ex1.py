a121e24ffeaebca3687cb65ef3d7880840e5811e
453slavchic <43350703+453slavchic@users.noreply.github.com>
2018-10-14 12:54:11 +0300

    Create ex1.py

---
+++ Homework2/ex1.py
@@ -0,0 +1,14 @@
+# Упражнение 1
+# Напишите программу, которая переставляет в обратном порядке символы второй половины строки
+
+def inversia(s):
+    if len(s) < 2:
+        return None
+    else:
+        s1 = s[: len(s)//2]
+        s2 = s[len(s) - 1 : len(s)//2 - 1 : -1]
+        s = s1 + s2
+        return (s)
+
+a = str(input())
+print(inversia(a))
